50d5b395be9c7f5d6c7121190a9566cadac69ccd
soulsilver142 <joelrojas2025@udec.cl>
2026-05-30 18:51:53 -0400

    modificar archivo main y ventana para tener PanelPrincipal

--- src/UI/Ventana.java
+++ src/UI/Ventana.java
@@ -8,15 +8,18 @@ public class Ventana extends JFrame {
         this.setTitle("esta Ventana");
         // se cambia Layout por defecto a BorderLayout
         this.setLayout(new BorderLayout());
+
         // se agregan botones no útiles para demostrar las zonas del Layout
         this.add(new JButton("sur"),BorderLayout.SOUTH);
-        this.add(new JButton("centro"),BorderLayout.CENTER);
         this.add(new JButton("norte"),BorderLayout.NORTH);
         this.add(new JButton("este"),BorderLayout.EAST);
         this.add(new JButton("oeste"),BorderLayout.WEST);
 
+        // se agrega el panel principal en el centro
+        this.add(new PanelPrincipal(), BorderLayout.CENTER);
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(400,300);
+        this.setSize(600,450);
         this.setVisible(true);
     }
 }
