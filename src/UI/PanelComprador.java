fcb186122f7b631dad65557a13e23b02ddf0d490
soulsilver142 <joelrojas2025@udec.cl>
2026-05-30 18:58:35 -0400

    implementar PanelPrincipal con PanelExpendedor y PanelComprador, ambos con métodos paintComponent de prueba

---
+++ src/UI/PanelComprador.java
@@ -0,0 +1,10 @@
+package UI;
+
+import java.awt.*;
+
+public class PanelComprador {
+    public void paintComponent(Graphics graphComprador) {
+        graphComprador.setColor(Color.blue);
+        graphComprador.drawOval(40,40,100,100);
+    }
+}
---
+++ src/UI/PanelExpendedor.java
@@ -0,0 +1,10 @@
+package UI;
+
+import java.awt.*;
+
+public class PanelExpendedor {
+    public void paintComponent(Graphics graphExpendedor) {
+        graphExpendedor.setColor(Color.red);
+        graphExpendedor.drawRect(320,250,80,80);
+    }
+}
