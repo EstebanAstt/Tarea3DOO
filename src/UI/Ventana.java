9e37deabfe1a1fb74cbced4a9894fd8cccc8b8d2
soulsilver142 <joelrojas2025@udec.cl>
2026-06-01 20:42:10 -0400

    agregar carpeta resources y actualizar Ventana y PanelComprador

--- src/UI/Ventana.java
+++ src/UI/Ventana.java
@@ -10,16 +10,17 @@ public class Ventana extends JFrame {
         this.setLayout(new BorderLayout());
 
         // se agregan botones no útiles para demostrar las zonas del Layout
+        /*
         this.add(new JButton("sur"),BorderLayout.SOUTH);
         this.add(new JButton("norte"),BorderLayout.NORTH);
         this.add(new JButton("este"),BorderLayout.EAST);
         this.add(new JButton("oeste"),BorderLayout.WEST);
+        */
 
-        // se agrega el panel principal en el centro
         this.add(new PanelPrincipal(), BorderLayout.CENTER);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(600,450);
+        this.setSize(800,680);
         this.setVisible(true);
     }
 }
