07361d83533ed826facba4c2d62c1b037931efa7
soulsilver142 <joelrojas2025@udec.cl>
2026-06-01 20:44:19 -0400

    actualizar PanelExpendedor con imagen de expendedor

--- src/UI/PanelExpendedor.java
+++ src/UI/PanelExpendedor.java
@@ -1,10 +1,16 @@
 package UI;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class PanelExpendedor {
     public void paintComponent(Graphics graphExpendedor) {
-        graphExpendedor.setColor(Color.red);
-        graphExpendedor.drawRect(320,250,80,80);
+        try {
+            ImageIcon expendedor = new ImageIcon("resources/Sprites/Expendedor.png");
+            Image fondo = expendedor.getImage();
+            graphExpendedor.drawImage(fondo,20,20,null);
+        } catch (Exception e) {
+            System.out.println("No se encontró el archivo de imagen");
+        }
     }
 }
