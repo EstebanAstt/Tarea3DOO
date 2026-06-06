51f6520083b7f2f8de398f7a54a40d1c840bc59b
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-06 01:56:55 -0400

    Prueba de PanelExpendedor

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,15 @@
 package Main;
+import UI.PanelExpendedor;
 import UI.Ventana;
+
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        new Ventana().setVisible(true);
+        // Swing requiere que las interfaces se ejecuten en su propio hilo seguro
+        SwingUtilities.invokeLater(() -> {
+            PanelExpendedor ventana = new PanelExpendedor();
+            ventana.setVisible(true);
+        });
     }
 }
