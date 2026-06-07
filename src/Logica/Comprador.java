ce6a41619a85aa2be3898fdebbb5a7b8767d1188
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-06 22:29:34 -0400

    Se modifico la clase Comprador, ahora en vez de crearse con una moneda se crea con una arraylist de monedas llamado monedero, esto para poder implementar correctamente la logica dentro de la UI

--- src/Logica/Comprador.java
+++ src/Logica/Comprador.java
@@ -1,15 +1,36 @@
 package Logica;
+import java.util.ArrayList;
 
 public class Comprador {
     private String sonido;
     private int vuelto;
+    private ArrayList<ArrayList<Moneda>> monedero;
 
     // agregar documentación a ésta clase
 
-    public Comprador(Moneda m, int tipo, Expendedor exp) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
+    public Comprador(int tipo, Expendedor exp) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
         this.vuelto = 0;
-        
-        exp.comprarProducto(m, tipo);
+        this.monedero = new ArrayList<>();
+
+        ArrayList<Moneda> monedasDe100  = new ArrayList<>();
+        ArrayList<Moneda> monedasDe500  = new ArrayList<>();
+        ArrayList<Moneda> monedasDe1000 = new ArrayList<>();
+        ArrayList<Moneda> monedasDe1500 = new ArrayList<>();
+
+        for(int i = 0; i <= 5; i++){
+            monedasDe100.add(new Moneda100());
+            monedasDe500.add(new Moneda500());
+            monedasDe1000.add(new Moneda1000());
+            monedasDe1500.add(new Moneda1500());
+
+        }
+
+        monedero.add(monedasDe100);
+        monedero.add(monedasDe500);
+        monedero.add(monedasDe1000);
+        monedero.add(monedasDe1500);
+
+
         Producto producto = exp.getProducto();
         
         // si se pudo comprar, se consume
@@ -35,4 +56,6 @@ public class Comprador {
     public String queConsumiste() {
         return this.sonido;
     }
+
+
 }
