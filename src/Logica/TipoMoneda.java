5c89b71c9b0cf7b2fd3f62d46e6cf10323fc350f
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-07 02:53:21 -0400

    Se utiliza Supplier para reducir codigo repetitivo

--- src/Logica/TipoMoneda.java
+++ src/Logica/TipoMoneda.java
@@ -1,22 +1,28 @@
 package Logica;
+import java.util.function.Supplier;
 
 public enum TipoMoneda {
 
-    MONEDA100(0),
-    MONEDA500(1),
-    MONEDA1000(2),
-    MONEDA1500(3);
+    MONEDA100(0, Moneda100::new),
+    MONEDA500(1, Moneda500::new),
+    MONEDA1000(2, Moneda1000::new),
+    MONEDA1500(3, Moneda1500::new);
 
 
     private final int tipo;
+    private final Supplier<Moneda> creador;
 
-
-    private TipoMoneda(int tipo) {
+    private TipoMoneda(int tipo, Supplier<Moneda> creador) {
         this.tipo = tipo;
+        this.creador = creador;
     }
 
 
     public int getTipo() {
         return this.tipo;
+    }
+
+    public Moneda crearMoneda() {
+        return this.creador.get();
     }
 }
