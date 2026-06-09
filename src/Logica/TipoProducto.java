f961312ad155a70bbcbb6e4a01b4c2a24c881d10
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-09 18:12:49 -0400

    se reordeno la distribucion de los productos

--- src/Logica/TipoProducto.java
+++ src/Logica/TipoProducto.java
@@ -2,13 +2,13 @@ package Logica;
 
 public enum TipoProducto {
     COCA(1000, 1),
-    SPRITE(1000, 2),
-    FANTA(1000, 3),
-    COCA1(1000,6 ),
-    SPRITE1(1000,7),
-    FANTA1(1000,8),
-    SUPER8(500, 4),
-    SNICKERS(1100, 5);
+    COCA1(1000,2 ),
+    SPRITE(1000, 3),
+    SPRITE1(1000,4),
+    FANTA(1000, 5),
+    FANTA1(1000,6),
+    SUPER8(500, 7),
+    SNICKERS(1100, 8);
 
     private int valor;
     private int tipo;
