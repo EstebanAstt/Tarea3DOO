b2184668f77843a58e0d9757aa61005d74f3bb8a
EstebanAstt <esteban.astetecif@gmail.com>
2026-06-09 14:39:25 -0400

    Se añadieron las bebidas duplicadas para el expendedor. Se añadio buscarPor tipo que funciona de la misma manera que el buscarPorTipo de TipoMoneda

--- src/Logica/TipoProducto.java
+++ src/Logica/TipoProducto.java
@@ -4,6 +4,9 @@ public enum TipoProducto {
     COCA(1000, 1),
     SPRITE(1000, 2),
     FANTA(1000, 3),
+    COCA1(1000,6 ),
+    SPRITE1(1000,7),
+    FANTA1(1000,8),
     SUPER8(500, 4),
     SNICKERS(1100, 5);
 
@@ -21,5 +24,15 @@ public enum TipoProducto {
 
     public int getTipo(){
         return tipo;
+    }
+
+    public static TipoProducto buscarPorTipo(int id) {
+        for (TipoProducto p : TipoProducto.values()) {
+            if (p.getTipo() == id) {
+                return p;
+            }
+        }
+
+        return null;
     }
 }
